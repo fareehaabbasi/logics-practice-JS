373e62148ae844ea3e5a1463e52691662b31c394
fareehaabbasi <abbasifareeha9@gmail.com>
2025-08-18 21:09:08 +0500

    question no:1

--- app.js
+++ app.js
@@ -6,5 +6,16 @@ var maximum69Number  = function(num) {
     }
     return num;
 };
-
 console.log(maximum69Number(9669));
+
+
+//EXAMPLE:2
+var maximum69Number  = function(num) {
+    let str = num.toString();
+    if(str.includes("6")) {
+        num = parseInt(str.replace("6", "9"));
+    }
+    return num;
+};
+
+console.log(maximum69Number(9969));
